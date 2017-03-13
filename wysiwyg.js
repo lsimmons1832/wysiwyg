17639d6c6d153856a330d86a06e0a16b6c7c8176
Latasha Simmons <lsimmons1832@gmail.com>
2017-03-12 21:04:10 -0500

    save work

--- wysiwyg.js
+++ wysiwyg.js
@@ -51,15 +51,17 @@ for (var i = 0; i < people.length; i++) {
 peopleHolder.innerHTML += peopleString;
 
 // When you click on one of the person elements, a dotted border should appear around it.
+// When you click on one of the person elements, the text input should immediately gain focus so that you can start typing.
 var section = document.getElementsByTagName("section");
+// var userInput = document.getElementsByClassName("userInput");
 	for (var j = 0; j < section.length; j++) {
 		console.log("I made it here",section);
 		section[j].addEventListener("click", function(){
 			this.classList.add("border");
+			document.getElementsByClassName("userInput").focus();
 	});
 }
 
 
-// When you click on one of the person elements, the text input should immediately gain focus so that you can start typing.
 // When there is a highlighted person element, and you begin typing in the input box, the person's biography should be immediately bound to what you are typing, letter by letter.
 // When you press the enter/return key when typing in the input field, then the content of the input field should immediately be blank.
