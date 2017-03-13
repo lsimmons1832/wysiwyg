8ed73a0cb01c6227cc3f3be6323fcd375e3c0029
Latasha Simmons <lsimmons1832@gmail.com>
2017-03-13 17:40:51 -0500

    remove dotted border from all items except the clicked element

--- wysiwyg.js
+++ wysiwyg.js
@@ -41,9 +41,9 @@ console.log(people);
 
 for (var i = 0; i < people.length; i++) {
 	currentPerson = people[i];
-	peopleString += `<div class="sectionContainer"><section id="section"><h3>${people[i].Name}</h3>`;
+	peopleString += `<div class="sectionContainer"><section class="section"><h3>${people[i].Name}</h3>`;
 	peopleString += `<h4>${people[i].Title}</h4>`;
-	peopleString += `<p>${people[i].Bio}</p>`;
+	peopleString += `<p class="bio">${people[i].Bio}</p>`;
 	peopleString += `<img src="${people[i].Image}">`;
 	peopleString += `<p>${people[i].Lifespan}</p></section>`;
 	peopleString += `</div>`;
@@ -56,19 +56,24 @@ var section = document.getElementsByClassName("sectionContainer");
 var userInput = document.getElementById("userInput");
 
 	for (var j = 0; j < section.length; j++) {
-		// console.log("I made it here", userInput);
 		section[j].addEventListener("click", function(){
+			var sectionContainer = document.getElementsByClassName("border");
+			for (var i = 0; i < sectionContainer.length; i++) {
+				sectionContainer[i].classList.remove("border");
+			}
 			this.classList.add("border");
 			userInput.focus();
-			// console.log("where am I?", section)
+			console.log("where am I?", section)
 			// for (var k = 0; k < userInput.length; k++) {
-			// 	userInput[k].addEventListener("keypress", function(event){
+			// 	userInput.addEventListener("keypress", function(event){
    //      	var mimicMeElement = document.getElementById("userInput").value;
- 		// 			event.target.parent.childNode.innerHTML = mimicMeElement;
+ 		// 			mimicMeElement.innerHTML = target.ParentNode.firstElementChild;
 			// 	});
 			// }
 	});
+	// this.classList.remove("border");
 }
+		// console.log("I made it here", firstElementChild);
 
 // When there is a highlighted person element, and you begin typing in the input box, the person's biography should be immediately bound to what you are typing, letter by letter.
 
